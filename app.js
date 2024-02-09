413548854d5aa26159c66083300a8377dd7a22fe
masde445 <matimasde.pl@gmail.com>
2024-02-09 19:51:08 +0100

    com2

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('views'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/submit', (req, res) => {
+  const { name, email } = req.body;
+  res.send(`Dane odebrane! Imię: ${name}, E-mail: ${email}`);
+});
+
+app.listen(port, () => {
+  console.log(`Aplikacja działa na http://localhost:${port}`);
+});
